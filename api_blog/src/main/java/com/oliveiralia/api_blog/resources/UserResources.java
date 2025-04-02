a3e4469000d6ae1073da6c59b69990aaa391f94d
lia-oliveira <dev.gillian@gmail.com>
2025-04-02 06:11:39 -0300

    Add function to fetch user posts

--- api_blog/src/main/java/com/oliveiralia/api_blog/resources/UserResources.java
+++ api_blog/src/main/java/com/oliveiralia/api_blog/resources/UserResources.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.oliveiralia.api_blog.domain.Post;
 import com.oliveiralia.api_blog.domain.User;
 import com.oliveiralia.api_blog.dto.UserDTO;
 import com.oliveiralia.api_blog.services.UserService;
@@ -60,6 +61,12 @@ public class UserResources {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();		
+	}
+	
+	@GetMapping(value="{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 				
 }
