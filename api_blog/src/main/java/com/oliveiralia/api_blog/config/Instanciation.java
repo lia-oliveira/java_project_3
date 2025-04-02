ce2f03535653dedd545fe1ea3d3928fefa5d785c
lia-oliveira <dev.gillian@gmail.com>
2025-04-01 21:57:31 -0300

    Add author data projection with DTO

--- api_blog/src/main/java/com/oliveiralia/api_blog/config/Instanciation.java
+++ api_blog/src/main/java/com/oliveiralia/api_blog/config/Instanciation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.oliveiralia.api_blog.domain.Post;
 import com.oliveiralia.api_blog.domain.User;
+import com.oliveiralia.api_blog.dto.AuthorDTO;
 import com.oliveiralia.api_blog.repositories.PostRepository;
 import com.oliveiralia.api_blog.repositories.UserRepository;
 
@@ -37,8 +38,8 @@ public class Instanciation implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Título do Post 1", "Conteúdo do post 1", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Título do Post 2", "Conteúdo do post 2", maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Título do Post 1", "Conteúdo do post 1", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Título do Post 2", "Conteúdo do post 2", new AuthorDTO(maria));
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
